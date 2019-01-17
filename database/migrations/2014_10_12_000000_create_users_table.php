8644872df662c284d5293432b651d33e52e76871
Dolwin Davis <imtrondev@gmail.com>
2019-01-17 15:00:57 +0530

    added initial view routes and files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,11 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            //$table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone');
+            $table->string('faculty');
+            $table->
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,35 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::get('/home', function () {
+    return view('home');
+});
+
+//Route::get('/home','ViewController@homeview');
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/student', function () {
+    return view('studentlist');
+});
+
+Route::get('/student/update', function () {
+    return view('studentupdate');
+});
+Route::get('/events/list', function () {
+    return view('eventlist');
+});
+Route::get('/events/details', function () {
+    return view('eventdetails');
+});
+
+
