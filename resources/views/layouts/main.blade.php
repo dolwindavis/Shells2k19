fe7ac9498f6d74cfff792b9ee2ad2c9a9988781a
Zapbitz <abhilash@three38inc.com>
2019-01-30 16:33:50 +0530

    addbtn updated

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,6 +18,8 @@
 
   <!--font awesome icon -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+  @yield('css-style')
 </head>
 <body>
 
