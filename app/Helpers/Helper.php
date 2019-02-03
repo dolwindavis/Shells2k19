a263ae86034dee7a7f2ff312807026eaf88b0a49
Dolwin Davis <imtrondev@gmail.com>
2019-02-03 16:14:12 +0530

    WIP Event Registration

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -42,7 +42,7 @@ class Helper
 
             }
         }
-        return $sevents;
+        return $events;
 
     }
 
--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Events;
 use App\Helpers\Helper;
+use App\Models\EventStudent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -51,8 +52,75 @@ class EventController extends Controller
 
     }
 
+
+    //register an event for the student
     public function eventRegister(Request $request)
     {
+        $studentid=$request->studentid;
+
+        $eventid=$request->eventid;
+
+        $user=Auth::user();
         
+        //evennt details
+        $event=Events::find($request->eventid);
+
+        //checking a student eligible for ragistering
+        for($i=0;$i<=count($studentid);$i++){
+
+            $eventstudent=EventStudent::where(['studentid',$studentid[$i]],['eventid',$eventid]);
+
+            //checking for exclusive event validation
+            if($event->exclusive == '1' && $eventstudent){
+
+                return response('false');
+
+            }
+
+        }
+
+        $eventstudent=EventStudent::where(['collegeid',$user->id],['eventid',$eventid])->get();
+
+        //checking college already registered or not
+        if($event->groupevent == '1' && $eventstudent){
+
+            return response('false');
+
+        }
+        //checking individual event registraion validation
+        elseif($event->maxnumber >= $eventstudent->count() && $event->groupevent == '0'){
+
+            return response('false');
+
+        }
+        //checking whether request has same number of students or not for an event
+        elseif($event->groupevent == '1' && (count($studentid) != $event->groupnumber)){
+
+            return response('false');
+
+        }
+
+        if($event->groupevent == '1'){
+
+            $request->groupid=str_random(4);
+
+        }
+        else{
+
+            $request->groupid='0';
+
+        }
+        
+
+        for($i=0;$i<=count($studentid);$i++){
+
+            $newevent=new EventStudent;
+
+            $newregs = $newevent->registerEventStudent($request);
+
+        }
+
+        return redirect('/events/list');
+
     }
 }
